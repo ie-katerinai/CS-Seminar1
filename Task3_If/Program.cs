28794c822d625b64ca386e1f3dfc9eb820a2e9f8
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2022-11-25 00:33:09 +0300

    Add Task 5.1

--- Task3_If/Program.cs
+++ Task3_If/Program.cs
@@ -2,12 +2,19 @@
 Console.WriteLine("Введите число от 1 до 7");
 int num = Convert.ToInt32(Console.ReadLine());
 string dayweek = "";//объявить как пустую строку
-if (num == 1) dayweek = "Понедельник";
-if (num == 2) dayweek = "Вторник";
-if (num == 3) dayweek = "Среда";
-if (num == 4) dayweek = "Четверг";
-if (num == 5) dayweek = "Пятница";
-if (num == 6) dayweek = "Суббота";
-if (num == 7) dayweek = "Воскресенье";
-Console.Write("Соответствующий день недели ");
-Console.WriteLine(dayweek);
+if (num > 0 && num < 8 )
+{
+    if (num == 1) dayweek = "Понедельник";
+    if (num == 2) dayweek = "Вторник";
+    if (num == 3) dayweek = "Среда";
+    if (num == 4) dayweek = "Четверг";
+    if (num == 5) dayweek = "Пятница";
+    if (num == 6) dayweek = "Суббота";
+    if (num == 7) dayweek = "Воскресенье";
+    Console.Write("Соответствующий день недели ");
+    Console.WriteLine(dayweek);
+}   
+else
+{
+    Console.WriteLine("Не корректное число ");
+}
--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая на вход принимает N, а на выходе выводит все числа от -N до N 
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int n = - num;
+while (n <= num)
+{
+    Console.Write(n + " " );
+    n++;
+}
